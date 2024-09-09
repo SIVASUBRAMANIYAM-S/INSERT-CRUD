fb1b4be045c43945edced0d9dc41a41218385550
SIVASUBRAMANIYAM-S <sivasaravanan492@gmail.com>
2024-09-09 13:26:52 +0530

    pos-insert-customer

--- customer/POS-insert-customer/lambda_function.py
+++ customer/POS-insert-customer/lambda_function.py
@@ -1,32 +1,32 @@
 import logging
 from DB_manager import DatabaseManager
+import pymysql
 import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    try:
-        conn = DatabaseManager.get_db_connection()   
+    try: 
+        conn = DatabaseManager.get_db_connection()
         try:
-            logger.info("Received event: %s", event)
+            print(event)
             customer_detail = json.loads(event['body'])
             
-            insert_customer_query = '''
-                INSERT INTO CUSTOMER (customerName, phoneNumber, email, address, state, country, postCode) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-            '''
-            
+            insert_customer_query = '''INSERT INTO CUSTOMER 
+                                       (shopId, customerName, phoneNumber, email, address, city, state, country, postCode) 
+                                       VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+                                       
             with conn.cursor() as cur:
-                customer_data = [
-                    customer_detail['customerName'],customer_detail['phoneNumber'],customer_detail['email'],customer_detail['address'],customer_detail['state'],customer_detail['country'],customer_detail['postCode']
-                ]
-                
+                customer_data = [customer_detail['shopId'], customer_detail['customerName'], customer_detail['phoneNumber'], 
+                                 customer_detail['email'], customer_detail['address'], customer_detail['city'], 
+                                 customer_detail['state'], customer_detail['country'], customer_detail['postCode']]
+                                 
                 cur.execute(insert_customer_query, customer_data)
-                customer_id = conn.insert_id()
+                customer_data_id = conn.insert_id()
                 conn.commit()
                 
-                customer_detail['customerId'] = customer_id
+                customer_detail['customerId'] = customer_data_id
                 
                 return {
                     "statusCode": 200,
@@ -42,9 +42,8 @@ def lambda_handler(event, context):
                         "response": customer_detail
                     }, default=str)
                 }
-                
+        
         except Exception as ex: 
-            logger.error("Error during database operation: %s", str(ex))
             return {
                 "statusCode": 502,
                 "headers": {
@@ -59,12 +58,12 @@ def lambda_handler(event, context):
                     "response": str(ex)
                 }, default=str)
             }
+        
         finally:
             if conn:
                 conn.close()
     
     except Exception as ex: 
-        logger.error("Error establishing database connection: %s", str(ex))
         return {
             "statusCode": 502,
             "headers": {
@@ -83,14 +82,15 @@ def lambda_handler(event, context):
 if __name__ == "__main__":
     events = {
         "body": json.dumps({
-            "customerName": "tharun",
-            "phoneNumber": "987324321",
-            "email": "tharunkumar@gmail.com",
-            "address": "ammapalayam",
-            "state": "tamilNadu",
+            "shopId": "1",
+            "customerName": "rahul",
+            "phoneNumber": "9894654423",
+            "email": "rahulvel123@gmail.com",
+            "address": "123 Main St",
+            "city": "chennai",
+            "state": "Tamil Nadu",
             "country": "India",
-            "postCode": 623621
+            "postCode": "623432"
         })
     }
-    response = lambda_handler(events, None)
-    print(response)
+    lambda_handler(events, None)
